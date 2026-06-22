f047ba53617e187f974ba183b660ac3c92536363
Younes Tsouli <younestsouli444@gmail.com>
2026-06-22 07:31:12 +0200

    feat: adding the navigation bar + styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
+import '../styles/reset.css'
 
 export default function Home() {
   return (
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,11 +1,45 @@
 import Link from "next/link";
+import "./Navbar.css";
+import { FiSearch, FiShoppingCart, FiUser } from "react-icons/fi";
 
 export default function Navbar() {
   return (
-    <nav>
-      <Link href="/">Home</Link>
-      <Link href="/shop">Shop</Link>
-      <Link href="/story">Story</Link>
+    <nav className="navbar">
+      {/* Gauche : Titre de la marque */}
+      <div className="signature">
+        <Link href="/" className="title">
+          The <span className="bee-span">Bee</span>hive
+        </Link>
+      </div>
+
+      {/* Centre : Liens de navigation principal */}
+      <ul className="nav-links">
+        <li>
+          <Link href="/story">Our Story</Link>
+        </li>
+        <li>
+          <Link href="/shop" className="active">Shop</Link>
+        </li>
+        <li>
+          <Link href="/provenance">Provenance</Link>
+        </li>
+      </ul>
+
+      {/* Droite : Barre de recherche et actions utilisateur */}
+      <div className="nav-actions">
+        <div className="search-container">
+          <FiSearch className="search-icon" />
+          <input type="text" placeholder="Find your honey..." />
+        </div>
+
+        <Link href="/cart" className="action-icon">
+          <FiShoppingCart />
+        </Link>
+
+        <Link href="/account" className="action-icon">
+          <FiUser />
+        </Link>
+      </div>
     </nav>
   );
 }
